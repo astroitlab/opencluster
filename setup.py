a17c1bcaff007a901e5e21f3abb03c79b0526d80
wsl <wsl@cnlab.net>
2014-07-02 14:54:28 +0800

    init

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='opencluster',
+    version='',
+    packages=[''],
+    package_dir={'': 'opencluster'},
+    url='',
+    license='',
+    author='wsl',
+    author_email='wsl@cnlab.net',
+    description=''
+)
