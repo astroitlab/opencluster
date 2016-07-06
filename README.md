78eaa9eb392eb641b68900bf711693e69c4194c0
Shoulin <wsl@cnlab.net>
2016-07-06 13:08:22 +0300

    redesign architecture,multiple running mode

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-Python Distributed Computing API
+OpenCluster  --   Python Distributed Computing API
 ===========
+1,Configuration:
+  mkdir -p /work/opencluster/logs
+  cp config.ini logging.conf /work/opencluster
+
+2,Extension
+    python helloManager.py -h
+   
--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from distutils.core import setup
 
 setup(
     name='opencluster',
-    version='',
-    packages=[''],
-    package_dir={'': 'opencluster'},
+    version='1.0',
+    packages=['opencluster'],
+    package_dir={'opencluster': 'opencluster'},
     url='',
     license='',
-    author='wsl',
+    author='Shoulin Wei',
     author_email='wsl@cnlab.net',
+    requires=['psutil','Pyro4','web.py'],
     description=''
 )
