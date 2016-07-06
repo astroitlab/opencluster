abb11095b8b794d8763b9027154408bfe5864ffb
Shoulin <wsl@cnlab.net>
2016-07-06 13:03:45 +0300

    redesign architecture,multiple running mode

--- opencluster/contractorservice.py
+++
@@ -1,8 +0,0 @@
-
-from worker import Worker
-class ContractorService(Worker) :
-    def __init__(self,ctor):
-        self.ctor = ctor
-
-    def doTask(self, inhouse):
-        return self.giveTask(inhouse)
